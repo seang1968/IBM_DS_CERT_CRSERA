9245664c2bf1557d893c22c43b203990d6e0fc13
seang1968 <sean.girgis@gmail.com>
2022-06-21 16:24:57 -0500

    env
    just env

---
+++ env.bat
@@ -0,0 +1,9 @@
+PATH=C:\pyver\394;C:\pyver\394\Scripts;%PATH%
+REM capstone\Scripts\activate
+
+REM  python -m ensurepip
+REM  python -m ensurepip --upgrade
+REM python -m pip install --upgrade pip
+REM pip install virtualenv
+REM python -m venv capstone
+REM python -m venv capstone
